b355391ba638d3a7a012d7e19e126733aa51423e
ShanuDey <sd4shanudey@gmail.com>
2022-11-04 13:07:38 +0530

    implement step definition methods

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -2,8 +2,12 @@ package StepDefinitions;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -32,22 +36,33 @@ public class LoginSteps {
 
 	@Given("I am on the login page")
 	public void i_am_on_the_login_page() {
-		System.out.println("Inside step - I am on the login page");
+		driver.navigate().to("https://shanudey.github.io/task-management-ui/#/login");
 	}
 
 	@When("^I enter email \"(.*)\" and password \"(.*)\"$")
 	public void i_enter_email_and_password(String email, String password) {
-		System.out.println("Inside step - I enter username and passowrd");
+		WebElement emailTextBox = driver.findElement(By.id("email"));
+		emailTextBox.sendKeys(email);
+		WebElement passwordTextBox = driver.findElement(By.id("password"));
+		passwordTextBox.sendKeys(password);
 	}
 
 	@When("I click on login button")
 	public void i_click_on_login_button() {
-		System.out.println("Inside step - I click on login button");
+		WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+		loginButton.click();
 	}
 
 	@Then("I verify dashbord page is opened")
-	public void i_verify_dashbord_page_is_opened() {
-		System.out.println("Inside step - I verify dashbord page is opened");
+	public void i_verify_dashbord_page_is_opened() throws Exception {
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+
+		String currentUrl = driver.getCurrentUrl();
+		if (!currentUrl.contains("dashboard")) {
+			System.out.println("Current URL = "+ currentUrl);
+			throw new Exception("Dashboard page is not opened");
+		}
 	}
 
 }

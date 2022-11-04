ce4a073a5a55b48bf7fb6cf5c81a881df573b3c8
ShanuDey <sd4shanudey@gmail.com>
2022-11-04 13:04:54 +0530

    open browser driver in before hook quit browser driver in after hook

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -1,5 +1,10 @@
 package StepDefinitions;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
@@ -7,16 +12,24 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginSteps {
+	WebDriver driver = null;
+
 	@Before
 	public void before() {
-		System.out.println("Inside before");
+		String driverPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", driverPath + "/src/test/resources/drivers/chromedriver.exe");
+
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
 	}
-	
+
 	@After
 	public void after() {
-		System.out.println("Inside after");
+		driver.quit();
 	}
-	
+
 	@Given("I am on the login page")
 	public void i_am_on_the_login_page() {
 		System.out.println("Inside step - I am on the login page");
